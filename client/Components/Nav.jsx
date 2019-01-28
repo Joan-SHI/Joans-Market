3eadbb5e79f1f6be87ae8c72ef1cc5e9d421e9b0
Joan-SHI <qiongnashi@gmail.com>
2019-01-29 09:50:01 +1300

    right-aligned nav bar

--- client/Components/Nav.jsx
+++ client/Components/Nav.jsx
@@ -16,11 +16,15 @@ const Nav = () =>{
         <a className="nav-link" href="https://www.linkedin.com/in/joan-shi-87a152179/">Contact</a>
         </li>
         </ul>
+        <ul className="nav navbar-nav navbar-right">
+      <li><span className="navbar-text" class="btn btn-warning">Joan's Market App</span></li>
+    </ul>
+
        {/*<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon">hello</span>
     </button>*/}
-       <div className="collapse navbar-collapse" id="navbarText">
-          {/*<ul className="navbar-nav mr-auto">
+       {/*<div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
               <a className="nav-link" href="https://github.com/Joan-SHI">Home <span class="sr-only">(current)</span></a>
             </li>
@@ -30,9 +34,9 @@ const Nav = () =>{
             <li className="nav-item">
               <a className="nav-link" href="qiongnashi@gmail.com">Contact</a>
             </li>
-    </ul>*/}
+    </ul>
           <span className="navbar-text-right" class="btn btn-warning">Joan's Market App</span>
-        </div>
+        </div>*/}
       </nav>
     )
 }
