3b974bdb476a0f743516502b9bbe818a9b630925
Joan-SHI <qiongnashi@gmail.com>
2019-01-29 08:48:14 +1300

    adjust the content in Market component

--- client/Components/Market.jsx
+++ client/Components/Market.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 const Market = () => {
   return (<div >
-    <h3>-- The factors which affect market rent:</h3>
+    <h4>-- The factors which affect market rent:</h4>
     <ul>
       <li>Bank Rate</li>
       <li>The rate of house price change</li>
       <li>Income rate of change</li>
       <li>Population growth rate</li>
     </ul>
-    <h3>-- Market Rent</h3>
-    <p>If bank rate increases, charge tennants more.</p>
-    <p>If bank rate decreases, you're already profiting.</p>
+    <h4>-- Market Rent</h4>
+    <ul>
+    <li>If bank rate increases, charge tennants more.</li>
+    <li>If bank rate decreases, you're already profiting.</li>
+    </ul>
     </div>
   )
 }
